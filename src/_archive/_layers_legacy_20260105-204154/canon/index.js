717e72dbbfaf1b665f4051408bd440bb428d90b8
Mike Slate <mikeslate77@icloud.com>
2026-01-05 21:25:10 -0500

    chore: untrack local legacy layer archives

--- src/_archive/_layers_legacy_20260105-204154/canon/index.js
+++
@@ -1,5 +0,0 @@
-export { CANON_LAYERS } from "./layers/index.js";
-export { CANON_DOCTRINES } from "./doctrines/index.js";
-
-export const getLayer = (n) => CANON_LAYERS.find((x) => x.number === n);
-export const listLayers = () => [...CANON_LAYERS].sort((a, b) => a.number - b.number);
